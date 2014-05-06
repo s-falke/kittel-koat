90e36da04869258d843ed6ee6dd75bc27ed6891e
Stephan Falke <falke@iti.uka.de>
2014-05-06 16:20:53 +0200

    Don't use tags.

--- make_git_sha1.sh
+++ make_git_sha1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 OUTFILE=$1
-GIT_SHA1=`git describe --always --dirty --abbrev=40`
+GIT_SHA1=`git describe --always --dirty --abbrev=40 --match=NeVeRmAtCh`
 
 cat > ${OUTFILE}.tmp << EOP
 let git_sha1 = "${GIT_SHA1}"
