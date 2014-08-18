2ec749831d08954086a8648cfda057870c97e618
Marc Brockschmidt <marc@marcbrockschmidt.de>
2014-08-18 21:49:03 +0100

    Fix copyright / description

--- utils/log.ml
+++ utils/log.ml
@@ -1,9 +1,9 @@
 (*
-  Main program for complexity analysis
+  Logging facilities for koat
 
-  @author Stephan Falke
+  @author Marc Brockschmidt
 
-  Copyright 2010-2014 Stephan Falke
+  Copyright 2014 Microsoft Research
 
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
