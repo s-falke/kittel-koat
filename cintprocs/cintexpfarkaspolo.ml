ee212b756e55f71f87504688ab5eb6d1a576c097
Marc Brockschmidt <mabrocks@microsoft.com>
2014-08-23 17:04:57 +0100

    Make cintexpfarkaspolo work again (and it's proven now)

--- cintprocs/cintexpfarkaspolo.ml
+++ cintprocs/cintexpfarkaspolo.ml
@@ -18,93 +18,101 @@
   limitations under the License.
 *)
 
-module CTRS = Ctrs.Make(Comrule)
+module CTRSObl = Ctrsobl.Make(Comrule)
+module CTRS = CTRSObl.CTRS
 module RVG = Rvgraph.Make(Comrule)
-module LSC = LocalSizeComplexity.Make(Comrule)
 module GSC = GlobalSizeComplexity.Make(Comrule)
+module LSC = GSC.LSC
 module TGraph = Tgraph.Make(Comrule)
 
-let first (x, _, _) =
-  x
+open CTRSObl
+open CTRS
 
 (* Find a polynomial interpretation *)
-let rec process degree (rcc, g, l) tgraph rvgraph =
-  if degree < 0 || degree > 1 || CTRS.isSolved rcc then
+let rec process ctrsobl tgraph rvgraph =
+  if CTRSObl.isSolved ctrsobl then
     None
   else
   (
-    let vars = Cintfarkaspolo.getVars rcc
-    and r = List.map first rcc
-    and s = List.map first (List.filter (fun (_, c, _) -> c = Complexity.Unknown) rcc) in
-      Farkaspolo.lambda_count := 0;
-      Farkaspolo.all_lambdas := [];
-      let toOrient = r in
-        let (abs, params) = Cintfarkaspolo.create_poly_map toOrient in
-          let cwbs = get_cwbs toOrient abs in
-            let cwbs_for_unknowns = Cintfarkaspolo.getOnlyFor cwbs toOrient s in
-              let weak = List.map Cintfarkaspolo.getAllWeak cwbs
-              and bound = List.map Cintfarkaspolo.getAllBound cwbs_for_unknowns in
-                let strictDecrease = List.map Cintfarkaspolo.getAllStrict (Cintfarkaspolo.getOnlyFor weak toOrient s) in
-                  let strict = Farkaspolo.combine bound (List.map List.flatten strictDecrease)
-                  and allparams = params @ !Farkaspolo.all_lambdas in
-                    let res = Smt.isSatisfiableFarkasPolo (List.flatten (List.flatten weak)) strict allparams in
-                      match res with
-                        | None -> None
-                        | Some model ->
-                          (
-                            let model' = Polo.fix_model model params in
-                              let conc = Polo.get_concrete_poly abs model' in
-                                let c = getC conc rcc g vars in
-                                  let nrcc = Cintfarkaspolo.annotate rcc s strict model' c
-                                  and ng = g
-                                  and nl = l
-                                  and ntgraph = tgraph
-                                  and nrvgraph = rvgraph in
-                                    if Cintfarkaspolo.equal rcc nrcc then
-                                      None
-                                    else
-                                      Some (((nrcc, ng, nl), ntgraph, nrvgraph), fun ini outi -> getProof ini outi (rcc, g, l) (nrcc, ng, nl) conc toOrient vars)
-                          )
-  )
+    Log.log (Printf.sprintf "Trying (Farkas-based) processor for exponential RFs ...");
+    let allCandidates = Cintfarkaspolo.getRuleSubsetsToOrient tgraph ctrsobl false in
+    Cfarkaspolo.findFirst (tryOneS ctrsobl tgraph rvgraph) allCandidates;
+  ) 
+and tryOneS ctrsobl tgraph rvgraph s =
+  Farkaspolo.lambda_count := 0;
+  Farkaspolo.all_lambdas := [];
+  let toOrient = ctrsobl.ctrs.rules in
+  let (abs, params) = Cintfarkaspolo.create_poly_map toOrient in
+  let cwbs_with_rules = get_cwbs toOrient abs in
+  let cwbs_with_rules_for_unknowns = Cintfarkaspolo.getOnlyFor cwbs_with_rules toOrient s in
+  let weak_with_rules = List.map Cintfarkaspolo.getAllWeak cwbs_with_rules in
+  let bound_with_rules = List.map Cintfarkaspolo.getAllBound cwbs_with_rules_for_unknowns in
+  let strictDecrease_with_rules = List.map Cintfarkaspolo.getAllStrict (Cintfarkaspolo.getOnlyFor weak_with_rules toOrient s) in
+  let boundedAndStrict_with_rules =
+    List.map2 
+      (fun (rB, b) (rS, ss) ->
+        assert(Comrule.equal rB rS);
+        (rB, b @ (List.flatten ss)))
+      bound_with_rules strictDecrease_with_rules
+  in
+  let allparams = params @ !Farkaspolo.all_lambdas in
+  let res = Smt.isSatisfiableFarkasPolo (List.flatten (Utils.concatMap snd weak_with_rules)) (List.map snd boundedAndStrict_with_rules) allparams in
+  match res with
+  | None -> None
+  | Some model ->
+    (
+      let model' = Polo.fix_model model params in
+      let conc = Polo.get_concrete_poly abs model' in
+      let c = getC conc ctrsobl in
+      let nctrsobl = Cintfarkaspolo.annotate ctrsobl boundedAndStrict_with_rules model' c in
+      if CTRSObl.haveSameComplexities ctrsobl nctrsobl then 
+        None
+      else
+        (
+          if Log.do_debug () then
+            Log.debug ("Found the following PRF:\n" ^ (Cintfarkaspolo.pol_to_string conc));
+          Log.log (Printf.sprintf "ExpRF synthesis successful, proven complexity %s." (Complexity.toString c));
+          Some ((nctrsobl, tgraph, rvgraph), getProof ctrsobl nctrsobl conc toOrient)
+        )     
+    )
+
 
 and get_cwbs toOrient abs =
-  List.map (fun r -> convert_rule_to_leqs r abs) toOrient
-and convert_rule_to_leqs (l, rs, c) abs =
-  let lpol = List.assoc (Term.getFun l) abs
-  and rpols = List.map (fun r -> List.assoc (Term.getFun r) abs) rs in
-    let lpolinst = Parapoly.instantiate lpol (Polo.getInstBin (Term.getFun l) (Term.getArgs l) 1)
-    and rpolinsts = List.map2 (fun r rpol -> Parapoly.instantiate rpol (Polo.getInstBin (Term.getFun r) (Term.getArgs r) 1)) rs rpols in
-      (
-        Farkaspolo.convert_constraint_to_leqs (Pc.dropNonLinearAtoms c),
-        List.map (fun rpolinst -> Parapoly.negate (Parapoly.minus lpolinst rpolinst)) rpolinsts,
-        Parapoly.add (Parapoly.negate lpolinst) ([], ([], Big_int.unit_big_int))
-      )
+  List.map (fun r -> (r, convert_rule_to_leqs r abs)) toOrient
+and convert_rule_to_leqs rule abs =
+  let lhs = Comrule.getLeft rule in
+  let rhss = Comrule.getRights rule in
+  let cond = Comrule.getCond rule in
+  let lpol = List.assoc (Term.getFun lhs) abs
+  and rpols = List.map (fun rhs -> List.assoc (Term.getFun rhs) abs) rhss in
+  let lpolinst = Parapoly.instantiate lpol (Polo.getInstBin (Term.getFun lhs) (Term.getArgs lhs) 1)
+  and rpolinsts = List.map2 (fun rhs rpol -> Parapoly.instantiate rpol (Polo.getInstBin (Term.getFun rhs) (Term.getArgs rhs) 1)) rhss rpols in
+  (
+    Farkaspolo.convert_constraint_to_leqs (Pc.dropNonLinearAtoms cond),
+    List.map (fun rpolinst -> Parapoly.negate (Parapoly.minus lpolinst rpolinst)) rpolinsts,
+    Parapoly.add (Parapoly.negate lpolinst) ([], ([], Big_int.unit_big_int))
+  )
 
-and getC conc rcc g vars =
+and getC conc ctrsobl =
   (* TODO: check correctness of this... *)
-  let pol_g = Expexp.fromPoly (List.assoc g conc)
-  and k = get_max_arity rcc in
-    let powterm = getPowTerm k pol_g in
-      let powterm_minus_one = Expexp.minus powterm Expexp.one in
-        Complexity.P (Expexp.abs (Expexp.instantiate powterm_minus_one (Cintfarkaspolo.getBindings vars 1)))
-and getPowTerm k pol =
-  if k = 2 then
-    let pol_plus_one = Expexp.add pol Expexp.one in
-      let k_pow_pol_plus_one = Expexp.exp (Expexp.fromConstant (Big_int.big_int_of_int 2)) pol_plus_one in
-        k_pow_pol_plus_one
-  else
-    let k_pow_pol = Expexp.exp (Expexp.fromConstant (Big_int.big_int_of_int k)) pol in
-      let twice_k_pow_pol = Expexp.mult (Expexp.fromConstant (Big_int.big_int_of_int 2)) k_pow_pol in
-        twice_k_pow_pol
-and get_max_arity rcc =
-  List.fold_left max 0 (List.map (fun (r, _, _) -> List.length (Comrule.getRights r)) rcc)
+  let pol_startFun = Expexp.fromPoly (List.assoc ctrsobl.ctrs.startFun conc) in
+  let b = get_max_rhs_arity ctrsobl.ctrs.rules in
+  let powterm = getPowTerm b pol_startFun in
+  let varBindings = List.mapi (fun i v -> ("X_" ^ (string_of_int (i + 1)), Expexp.fromVar v)) (CTRS.getVars ctrsobl.ctrs) in
+  Complexity.P (Expexp.abs (Expexp.instantiate powterm varBindings))
+and getPowTerm b pol =
+  (* Bound should be b * (b^pol - 1))/(b-1). We approximate as b^(pol + 1). *)
+  let pol_plus_one = Expexp.add pol Expexp.one in
+  Expexp.exp (Expexp.fromConstant (Big_int.big_int_of_int b)) pol_plus_one
+and get_max_rhs_arity rules =
+  List.fold_left max 0 (List.map (fun r -> List.length (Comrule.getRights r)) rules)
 
-and getProof ini outi rccgl nrccgl pol toOrient vars =
-  let newlybound = List.filter (Cintfarkaspolo.isNewlyBound (first rccgl)) (first nrccgl) in
-    let more = (List.length newlybound) <> 1 in
-      "A polynomial rank function for exponential bounds with\n" ^
-      (Cintfarkaspolo.pol_to_string pol) ^ "\n" ^
-      "orients all transitions weakly and the " ^ (if more then "transitions" else "transition") ^ "\n" ^
-      (CTRS.toStringPrefix "\t" newlybound) ^ "\n" ^
-      "strictly and produces the following problem:\n" ^
-      (CTRS.toStringGNumber nrccgl outi)
+and getProof oldctrsobl newctrsobl pol toOrient ini outi =
+  let newlybound = List.filter (fun rule -> not (CTRSObl.hasUnknownComplexity newctrsobl rule)) (CTRSObl.getUnknownComplexityRules oldctrsobl) in
+  let moreThanOne = (List.length newlybound) <> 1 in
+  "A polynomial rank function for exponential bounds with\n" ^
+    (Cintfarkaspolo.pol_to_string pol) ^ "\n" ^
+    "orients all transitions weakly and the " ^ (if moreThanOne then "transitions" else "transition") ^ "\n" ^
+    (Comrule.listToStringPrefix "\t" newlybound) ^ "\n" ^
+    "strictly and produces the following problem:\n" ^
+    (CTRSObl.toStringNumber newctrsobl outi)
