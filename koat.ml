86283c975f9ae93e8be0a220d0da5cf93e9b7455
Stephan Falke <stephan.falke@gmail.com>
2014-09-05 20:56:23 +0200

    Mention Z3 internal in the help output.

--- koat.ml
+++ koat.ml
@@ -90,7 +90,8 @@ let rec speclist =
     ("--combine", Arg.String (fun s -> combine := stringToCombine s print_usage), "");
     ("-timeout", Arg.Set_float timeout, Printf.sprintf "     - Set the timeout (seconds, 0 = no timeout) [default %.2f]" !timeout);
     ("--timeout", Arg.Set_float timeout, "");
-    ("-smt-solver", Arg.String (fun s -> Smt.setSolver (checkSmtSolver s print_usage)), "  - Set the SMT solver (yices/yices2/z3/cvc4/mathsat5)");
+    ("-smt-solver", Arg.String (fun s -> Smt.setSolver (checkSmtSolver
+s print_usage)), "  - Set the SMT solver (" ^ (String.concat "/" supportedSmtSolvers) ^ ")");
     ("--smt-solver", Arg.String (fun s -> Smt.setSolver (checkSmtSolver s print_usage)), "");
     ("-max-chaining", Arg.Set_int maxchaining, Printf.sprintf "- Set the maximum number of chaining processor applications [default %i]" !maxchaining);
     ("--max-chaining", Arg.Set_int maxchaining, "");
